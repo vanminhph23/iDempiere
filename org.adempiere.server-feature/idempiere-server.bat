f2971993b3ac7a7da9411477f6af7064d482af0d
globalqss <globalqss@localhost>
2015-06-25 14:50:22 -0500

    IDEMPIERE-2698 Performance: Report checking for zoom condition on database - must be cached

---
+++ org.adempiere.server-feature/idempiere-server.bat
@@ -0,0 +1,21 @@
+@Echo off
+
+
+@if not "%JAVA_HOME%" == "" goto JAVA_HOME_OK
+@Set JAVA=java
+@Echo JAVA_HOME is not set.
+@Echo You may not be able to start the server
+@Echo Set JAVA_HOME to the directory of your local 1.6 JDK.
+goto START
+
+:JAVA_HOME_OK
+@Set JAVA=%JAVA_HOME%\bin\java
+
+
+:START
+@Echo =======================================
+@Echo Starting iDempiere Server ...
+@Echo =======================================
+
+FOR %%c in (plugins\org.eclipse.equinox.launcher_1.*.jar) DO set JARFILE=%%c
+@"%JAVA%" -Dosgi.compatibility.bootdelegation=true -Dosgi.noShutdown=true -Dosgi.framework.activeThreadType=normal -XX:MaxPermSize=192m -jar %JARFILE% -console 12612 -application org.adempiere.server.application
